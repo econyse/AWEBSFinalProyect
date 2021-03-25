3a99f93a49012e132fd0ccdea0162f99a7c4fca0
econyse <48955800+econyse@users.noreply.github.com>
2021-03-25 10:49:15 -0500

    Update config.js

--- Server/firebase/config.js
+++ Server/firebase/config.js
@@ -3,12 +3,12 @@ require('firebase/auth');
 require('firebase/firestore')
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA22TIrZa-rOY-at7yonbwNb9pOEvbpySo",
-    authDomain: "finalproject-67cb8.firebaseapp.com",
-    projectId: "finalproject-67cb8",
-    storageBucket: "finalproject-67cb8.appspot.com",
-    messagingSenderId: "885111161110",
-    appId: "1:885111161110:web:f20df485f1bc117a225037"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
